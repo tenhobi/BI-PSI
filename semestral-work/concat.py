49b0f2511c7a87af503121eb1f28effd502c712c
Honza Bittner <git@honzabittner.cz>
2019-05-05 16:49:40 +0200

    Finish

--- semestral-work/concat.py
+++ semestral-work/concat.py
@@ -1,8 +1,8 @@
 import glob2
 
-filenames = glob2.glob('*.py')  # list of all .txt files in the directory
+filenames = glob2.glob('**/*.py')  # list of all .txt files in the directory
 
-with open('all-merged.txt', 'w') as f:
+with open('all.txt', 'w') as f:
     for file in filenames:
         with open(file) as infile:
             f.write(infile.read() + '\n')
